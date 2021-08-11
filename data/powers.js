23bc26a85a412fdf637e11c763fbaa741fff2dfc
Aemilius Morgan <aemiliusmorgan@gmail.com>
2021-08-11 13:39:05 -0700

    adding setup

---
+++ data/powers.js
@@ -0,0 +1,39 @@
+const data = [
+  {
+    id: 1,
+    name: 'Super Speed',
+    desc: 'Able to run near or at speed of light',
+    realistic: false,
+    type: 'physical'
+  },
+  {
+    id: 2,
+    name: 'Fly',
+    desc: 'To be able to fly',
+    realistic: true,
+    type: 'teleknetic'
+  },
+  {
+    id: 3,
+    name: 'Eternal-life',
+    desc: 'Live forever',
+    realistic: false,
+    type: 'physical'
+  },
+  {
+    id: 4,
+    name: 'Give life',
+    desc:'be able to give life to inanimate objects',
+    realistic: true,
+    type: 'super natural'
+  }, 
+  {
+    id: 5,
+    name: 'Heal through time reversion',
+    desc: 'Be able to heal anything by turning back time in a specific area',
+    realistic: true,
+    type: 'space/time'
+  }
+];
+
+module.exports = data;
